698273faea234116ccee46cf280aec106d4d42d5
m1tywaflow <dimamarchuk2708@gmail.com>
2025-03-27 17:47:16 +0200

    update

--- src/components/TestimonialsSlider.jsx
+++ src/components/TestimonialsSlider.jsx
@@ -92,18 +92,48 @@ import girl1 from "../assets/girl1.jpg";
 import girl2 from "../assets/girl2.jpg";
 import girl3 from "../assets/girl3.jpg";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
-import { wrap } from 'motion';
+import { wrap } from "motion";
 
 const testimonials = [
-  { name: "Max", role: "Client", image: men1, text: "The best service I have ever used!" },
-  { name: "Ann", role: "Client", image: girl1, text: "Very convenient and reliable service!" },
+  {
+    name: "Max",
+    role: "Client",
+    image: men1,
+    text: "The best service I have ever used!",
+  },
+  {
+    name: "Ann",
+    role: "Client",
+    image: girl1,
+    text: "Very convenient and reliable service!",
+  },
   { name: "Alex", role: "Client", image: men2, text: "100% satisfied!" },
-  { name: "Kate", role: "Client", image: girl2, text: "Super high quality service!" },
+  {
+    name: "Kate",
+    role: "Client",
+    image: girl2,
+    text: "Super high quality service!",
+  },
   { name: "John", role: "Client", image: men3, text: "Wonderful experience!" },
-  { name: "Mary", role: "Client", image: girl3, text: "Fast, convenient, I recommend!" },
+  {
+    name: "Mary",
+    role: "Client",
+    image: girl3,
+    text: "Fast, convenient, I recommend!",
+  },
   { name: "Bobi", role: "Client", image: men4, text: "Amazing impression!" },
-  { name: "Charlz", role: "Client", image: men5, text: "Fast, easy, highly recommended!" },
-  { name: "JeryXd", role: "Client", image: men6, text: "Top-quality customer experience!" },
+  {
+    name: "Charlz",
+    role: "Client",
+    image: men5,
+    text: "Fast, easy, highly recommended!",
+  },
+  {
+    name: "JeryXd",
+    role: "Client",
+    image: men6,
+    text: "Top-quality customer experience!",
+  },
 ];
 
 const TestimonialsSlider = () => {
@@ -117,55 +147,47 @@ const TestimonialsSlider = () => {
   };
 
   return (
-    <div className="relative w-full max-h-1200 bg-gray-700 py-10">
-      <h2 className="text-3xl font-bold mb-6 text-center text-amber-100">Reviews</h2>
+    <div className="relative w-full  bg-gray-700 py-10">
+      <h2 className="text-3xl font-bold mb-6 text-center text-amber-100">
+        Reviews
+      </h2>
       <div className="relative w-full">
-      <AnimatePresence initial={false} custom={direction}>
-  <motion.div
-    key={selectedItem}
-    initial={{ opacity: 0, x: direction === 1 ? 100 : -100, height: "auto" }} 
-    animate={{
-      opacity: 1,
-      x: 0,
-      height: "auto",  
-      transition: {
-        delay: 0.2,
-        type: "spring",
-        duration: 0.3,
-        bounce: 0.4,
-      },
-    }}
-    exit={{
-      opacity: 0,
-      x: direction === 1 ? -100 : 100,
-      height: "auto",  
-      transition: {
-        type: "spring",
-        duration: 0.5,
-        bounce: 0.3,
-      },
-    }}
-    className="flex justify-center items-center"
-  >
-    <div className="text-center bg-gray-700 p-8 rounded-lg shadow-lg">
-      <img
-        src={testimonials[selectedItem].image}
-        alt={testimonials[selectedItem].name}
-        className="w-80 h-80 mx-auto rounded-lg object-cover"
-      />
-      <p className="text-xl font-semibold mt-4 text-amber-100">
-        {testimonials[selectedItem].text}
-      </p>
-      <h3 className="text-2xl font-bold mt-2 text-amber-100">
-        {testimonials[selectedItem].name}
-      </h3>
-      <p className="text-amber-100 text-lg">{testimonials[selectedItem].role}</p>
-    </div>
-  </motion.div>
-</AnimatePresence>
+        <div className="flex justify-center items-center">
+          <AnimatePresence initial={false} custom={direction} mode="popLayout">
+            <motion.div
+            key={testimonials[selectedItem].name}
+              initial={{ opacity: 0, x: direction * 50 }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  delay: 0.2,
+                  type: "spring",
+                  visualDuration: 0.3,
+                  bounce: 0.4,
+                },
+              }}
+              exit={{ opacity: 0, x: direction * -50 }}
+              className="text-center bg-gray-700 p-8 rounded-lg shadow-lg"
+            >
+              <img
+                src={testimonials[selectedItem].image}
+                alt={testimonials[selectedItem].name}
+                className="w-80 h-80 mx-auto rounded-lg object-cover"
+              />
+              <p className="text-xl font-semibold mt-4 text-amber-100">
+                {testimonials[selectedItem].text}
+              </p>
+              <h3 className="text-2xl font-bold mt-2 text-amber-100">
+                {testimonials[selectedItem].name}
+              </h3>
+              <p className="text-amber-100 text-lg">
+                {testimonials[selectedItem].role}
+              </p>
+            </motion.div>
+          </AnimatePresence>
+        </div>
 
-
-     
         <button
           onClick={() => setSlide(-1)}
           className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-md"
